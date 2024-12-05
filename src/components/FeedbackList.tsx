06a31ca30c767d072a148f905a543277a6c09227
Álvaro Troya <alvaro.troya.ec@googlemail.com>
2024-12-05 11:06:43 +0100

    Add loading spinner

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -6,20 +6,26 @@ const API_URL = "https://bytegrad.com/course-assets/projects/corpcomment/api/fee
 
 export default function FeedbackList() {
   const [feedbackItems, setFeedbackItems] = useState<FeedbackItem[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
     const fetchFeedback = async () => {
       // I intentionally left out the error handling for brevity, we should use react-query for this
       const response = await fetch(API_URL);
       const data = await response.json();
       setFeedbackItems(data.feedbacks);
+      setIsLoading(false);
     };
     fetchFeedback();
   }, []);
 
   return (
-    <ol className="feedback-list">
-      {feedbackItems.map((item) => <FeedbackItemListEntry {...item} />)}
-    </ol>
+    <>
+      {isLoading && <div className="spinner" aria-hidden="true" />}
+      <ol className="feedback-list">
+        {feedbackItems.map((item) => <FeedbackItemListEntry {...item} />)}
+      </ol>
+    </>
   );
 }
